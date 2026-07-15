8f97d9185ff87a2cdd8a9350c49ba8bfa6a9b296
Timur <timurcha7474@gmail.com>
2026-07-15 23:32:47 +0400

    Add project files

---
+++ pages/form_pages.py
@@ -0,0 +1,35 @@
+from pages.base_page import BasePage
+
+class FormPage (BasePage):
+    def navigate (self):
+        self.page.goto("https://moskva.beeline.ru/spam-feedback/")
+
+    def fill_name (self, name):
+        self.page.locator('[name="person"]').fill(name)
+
+    def fill_date (self, date):
+        self.page.locator('[name="callStart"]').fill(date)
+
+    def fill_phone (self, phone):
+        self.page.locator('[name="ctnB"]').fill(phone)
+
+    def select_sms (self):
+        self.page.get_by_role("checkbox", name="СМС").check()
+
+    def fill_sender (self, sender):
+        self.page.locator('[name="ctnA"]').fill(sender)
+
+    def fill_description (self, description):
+        self.page.get_by_role("textbox", name="Описание").fill(description)
+
+    def accept_policy (self):
+        self.page.get_by_role("checkbox", name="Нажимая кнопку «Отправить», вы подтверждаете, что ознакомились с Политикой «Обработка персональных данных «ПАО «ВымпелКом».").check()
+
+    def click_sumbit(self):
+        self.page.get_by_role("button", name="Отправить").click()
+
+    def success_message(self):
+        return self.page.get_by_text("спасибо за информацию!")
+
+
+
---
+++ tests/test_form.py
@@ -0,0 +1,38 @@
+import pytest
+import allure
+from playwright.sync_api import expect
+from pages.form_pages import FormPage
+
+
+@allure.title("Успешная отправка формы")
+def test_success_send_sms(page):
+    form = FormPage(page)
+
+    form.navigate()
+    form.fill_name("Иванов Иван Иванович")
+    form.fill_date("12.07.2026")
+    form.fill_phone("+79371234567")
+    form.select_sms()
+    form.fill_sender("+79377654321")
+    form.fill_description("Реклама")
+    if not page.locator("comment").is_visible(): pytest.fail("Поле 'Комментарий' не отображается на странице.")
+    form.accept_policy()
+
+    form.click_sumbit()
+
+    expect(page.get_by_role("button", name="Отправить")).to_be_enabled()
+
+
+@allure.title("Отправка формы без огласия")
+def test_send_without_policy(page):
+    form = FormPage(page)
+
+    form.navigate()
+    form.fill_name("Иванов Иван Иванович")
+    form.fill_date("12.07.2026")
+    form.fill_phone("+79371234567")
+    form.select_sms()
+    form.fill_sender("+79377654321")
+    form.fill_description("Реклама")
+
+    expect(page.get_by_role("button", name="Отправить")).to_be_disabled()
